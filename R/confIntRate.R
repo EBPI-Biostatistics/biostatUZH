df44faa1185733fe632955938208a6ec1f53dfd3
Florian Gerber <flora.fauna.gerber@gmail.com>
2021-08-10 09:55:16 +0200

    confIntRate*

--- R/confIntRate.R
+++ R/confIntRate.R
@@ -1,53 +1,136 @@
-#' Compute confidence interval for a Poisson rate via different methods
+#' Wald and Wilson confidence intervals for Poisson rates
 #' 
-#' Compute a confidence interval for a Poisson rate using several methods. The
-#' individual methods are also available as separate functions \code{waldRate}
-#' and \code{wilsonRate}.
-#' 
+#' Compute a confidence interval for Poisson rates using the Wald and Wilson methods. 
 #' 
 #' @aliases confIntRate waldRate wilsonRate
+#' @rdname confIntRate
 #' @param x Number of events.
 #' @param t Total observation time.
 #' @param conf.level Confidence level for confidence interval.
-#' @return A list with the entries: \item{p}{Estimated rate.}
-#' \item{CIs}{Dataframe containing the estimated confidence intervals.}
+#' @return \code{confIntRate} returns a data.frame with columns: \code{x}, \code{t}, \code{type},
+#' \code{rate}, \code{lower}, \code{upper}.
 #' @author Leonhard Held
-#' @seealso Functions for some of the intervals provided here are available in
-#' \pkg{Hmisc} (see the examples).
+#' @seealso \code{\link{confIntRateDiff}}, package \pkg{Hmisc}.
 #' @references Held, L., Rufibach, K. and Seifert, B. (2013). Medizinische
-#' Statistik - Konzepte, Methoden, Anwendungen.  Section 8.2.
+#' Statistik - Konzepte, Methoden, Anwendungen. Section 8.2.
 #' @examples
 #' 
-#' ## Calculate confidence bounds for a Poisson rate by different methods. 
-#' x <- 1
-#' t <- 3
-#' ci <- confIntRate(x, t)$CIs
-#' ci
-#' p <- confIntRate(x, t)$rate
+#' confIntRateWald(x = 1, t = 2.2)
+#' confIntRateWilson(x = 1, t = 2.2)
+#' confIntRate(x = 3:1, t = seq(from = 1, to = 2.2, length.out = 3))
 #' 
-#' plot(0, 0, type = 'n', ylim = c(0, 3), xlim = c(0, 3), xlab = 'p', 
-#'     ylab = '', yaxt = 'n')
-#' lines(ci[1, 2:3], c(1, 1))
-#' lines(ci[2, 2:3], c(2, 2))
-#' points(p, 1, pch=19)
-#' points(p, 2, pch=19)
-#' text(0.5, 0.85, 'wald')
-#' text(0.5, 1.85, 'wilson')
-#'
 #' @export
 confIntRate <- function(x, t, conf.level = 0.95)
 {
-    stopifnot(is.wholenumber(x), (t>0),  conf.level<1, conf.level>0)
-
-    res <- data.frame(matrix(NA, ncol = 3))
-    colnames(res) <- c("type", "lower", "upper")
-
-    res[1, 2:3] <- waldRate(x, t, conf.level = conf.level)[c(1, 3)]
-    res[2, 2:3] <- wilsonRate(x, t, conf.level = conf.level)[c(1, 3)]
-
-    res[, 1] <- c("Wald", "Wilson")
-
-    res <- list("rate" = x / t, "CIs" = res)
-    return(res)
-
+    ## input is checked in 'confIntRateWald' and 'confIntRateWilson'
+    rbind(data.frame(x = x, t = t, type = "Wald",
+                     confIntRateWald(x = x, t = t, conf.level = conf.level)),
+          data.frame(x = x, t = t, type = "Wilson",
+                     confIntRateWilson(x = x, t = t, conf.level = conf.level)))
 }
+
+
+#' @rdname confIntRate
+#' @return \code{confIntRateWald}returns a matrix with columns
+#' \code{rate}, \code{lower}, and \code{upper}.
+#' @export
+confIntRateWald <- function(x, t, conf.level = 0.95) {
+    stopifnot(is.numeric(x), length(x) > 0,
+              is.finite(x), is.wholenumber(x),
+              is.numeric(t), length(t) > 0,
+              is.finite(t), t > 0,
+              is.numeric(conf.level), length(conf.level) == 1,
+              is.finite(conf.level),
+              0 < conf.level, conf.level < 1)
+    lambda <- x / t
+    q <- qnorm(p = (1 + conf.level) / 2)
+    ef <- exp(q / sqrt(x))
+    cbind("rate" = lambda, "lower" = lambda / ef, "upper" = lambda * ef)
+}
+
+#' @export
+waldRate <- function(x, t, conf.level = 0.95){
+    .Deprecated(new = "confIntRateWald")
+    confIntRateWald(x = x, t = t, conf.level = conf.level)
+}
+
+
+#' @rdname confIntRate
+#' @return \code{confIntRateWilson} returns a matrix with columns
+#' \code{rate}, \code{lower}, and \code{upper}.
+#' @export
+confIntRateWilson <- function(x, t, conf.level = 0.95) {
+    stopifnot(is.numeric(x), length(x) > 0,
+              is.finite(x), is.wholenumber(x),
+              is.numeric(t), length(t) > 0,
+              is.finite(t), t > 0,
+              is.numeric(conf.level), length(conf.level) == 1,
+              is.finite(conf.level),
+              0 < conf.level, conf.level < 1)
+    lambda <- x / t
+    q <- qnorm(p = (1 + conf.level) / 2)
+    a <- x + q^2 / 2
+    b <- q / 2 * sqrt(4 * x + q^2)
+    cbind("rate" = lambda, "lower" = (a - b) / t,
+          "upper" = (a + b) / t)
+}
+
+#' @export
+wilsonRate <- function(x, t, conf.level = 0.95){
+    .Deprecated(new = "confIntRateWald")
+    confIntRateWilson(x = x, t = t, conf.level = conf.level)
+}
+
+
+#' Wald and Wilson confidence intervals for Poisson rates differences
+#' 
+#' Compute a confidence interval for Poisson rate differences using the
+#' Wald and Wilson methods. 
+#' 
+#' @param x vector of length 2, number of events in each group.
+#' @param t vector of length 2, total observation time in each group.
+#' @param conf.level Confidence level for confidence interval.
+#' @return A list with the entries:
+#' \item{rd}{Estimated rate difference.}
+#' \item{CIs}{Data.frame containing confidence intervals for the rate difference.}
+#' @author Leonhard Held
+#' @seealso \code{\link{confIntRateWilson}}, \code{\link{confIntRateWald}}
+#' @references Held, L., Rufibach, K. and Seifert, B. (2013). Medizinische
+#' Statistik - Konzepte, Methoden, Anwendungen. Section 8.2.
+#' @keywords htest
+#' @examples
+#' 
+#' confIntRateDiff(x = c(30, 50), t = c(100, 120.1))
+#' 
+#' @export
+confIntRateDiff <- function(x, t, conf.level = 0.95){
+    stopifnot(is.numeric(x), length(x) == 2,
+              is.finite(x), is.wholenumber(x),
+              is.numeric(t), length(t) == 2,
+              is.finite(t), t > 0,
+              is.numeric(conf.level), length(conf.level) == 1,
+              is.finite(conf.level),
+              0 < conf.level, conf.level < 1)
+    
+    ci1 <- confIntRateWilson(x = x[1], t = t[1], conf.level = conf.level)
+    ci2 <- confIntRateWilson(x = x[2], t = t[2], conf.level = conf.level)
+
+    diff <- matrix(ci1[1] - ci2[1])
+    se.diff <- sqrt(x[1] / t[1]^2 + x[2] / t[2]^2)
+    z <- qnorm((1 + conf.level) / 2)
+    wald.lower <- diff - z * se.diff
+    wald.upper <- diff + z * se.diff
+    score.lower <- diff - sqrt((ci1[1] - ci1[2])^2 + (ci2[3] - ci2[1])^2)
+    score.upper <- diff + sqrt((ci2[1] - ci2[2])^2 + (ci1[3] - ci1[1])^2)
+
+    result <- matrix(ncol = 2, nrow = 2)
+    result[,1] <- c(wald.lower, score.lower)
+    result[,2] <- c(wald.upper, score.upper)
+
+    out <- data.frame(type = c("Wald", "Wilson"), result)
+    names(out) <- c("type", "lower", "upper")
+
+    list("rd" = diff, "CIs" = out)
+}
+
+
