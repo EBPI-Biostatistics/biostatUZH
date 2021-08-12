041fff847e743efb059e61d22ee7f13db5557c44
Florian Gerber <flora.fauna.gerber@gmail.com>
2021-08-12 10:36:59 +0200

    clean up lamAlpha rootFct

--- R/confIntICC.R
+++ R/confIntICC.R
@@ -222,3 +222,18 @@ computeICCrater <- function(dat)
     return(res)
 }
 
+lamAlpha <- function(alpha, n, d, MSr, MSe){
+    
+    ## compute the function A(alpha) on p. 621 of Roussen et al (2003)
+    limit <- c(10^-5, 1000)
+    if (sum(abs(sign(rootFct(limit, alpha, n, d, MSr, MSe)))) == 2){lam.a <- 0} else {
+        lam.a <- uniroot(rootFct, interval = limit, alpha, n, d, MSr, MSe)$root}
+    return(lam.a)    
+}
+
+rootFct <- function(x, alpha, n, d, MSr, MSe){
+
+    Fa <- qf(1 - alpha, df1 = d - 1, df2 = (n - 1) * (d - 1), ncp = x)
+    res <- Fa - MSr / MSe
+    return(res)
+}
