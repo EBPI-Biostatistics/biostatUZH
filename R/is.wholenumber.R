4c4f74926e6d7e3edf1b0ad9ab0729bd000eb35c
Florian Gerber <flora.fauna.gerber@gmail.com>
2021-08-16 10:00:51 +0200

    is.wholenumber.R

--- R/is.wholenumber.R
+++ R/is.wholenumber.R
@@ -7,10 +7,15 @@
 #' @param tol Numeric vector of length 1 specifying the tolerance.
 #' Default is \code{.Machine$double.eps^0.5}.
 #' @examples
-#' is.wholenumber(5L)
-#' is.wholenumber(5)
-#' is.wholenumber(5 + .Machine$double.eps^0.5 / 2)
-#' is.wholenumber(5 + .Machine$double.eps^0.5)
+#' is.wholenumber(1L:5L)
+#' is.wholenumber(1:5)
+#' is.wholenumber(1:5 + .Machine$double.eps^0.5 / 2)
+#' is.wholenumber(1:5 + .Machine$double.eps^0.5)
 #' @export
-is.wholenumber <- function (x, tol = .Machine$double.eps^0.5)
+is.wholenumber <- function (x, tol = .Machine$double.eps^0.5){
+    stopifnot(is.numeric(x), length(x) >= 1,
+              is.finite(x),
+              is.numeric(tol), length(tol) == 1,
+              is.finite(tol), tol > 0)
     abs(x - round(x)) < tol
+}
