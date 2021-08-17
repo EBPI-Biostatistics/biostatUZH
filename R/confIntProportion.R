0491c08220340d919b10095c68a8038b1541fc09
Florian Gerber <flora.fauna.gerber@gmail.com>
2021-08-17 09:50:21 +0200

    confIntPairedProportion

--- R/confIntProportion.R
+++ R/confIntProportion.R
@@ -203,8 +203,8 @@ clopperPearson <- function(x, n, conf.level = 0.95)
 #' \item{waldCI}{Wald confidence interval for the difference of independent
 #' proportions.}
 #' @author Leonhard Held
-#' @seealso \code{\link{confIntProportion}}, \code{\link[stats]{fisher.test}},
-#' \code{\link[exact2x2]{exact2x2}}
+#' @seealso \code{\link{confIntPairedProportion}}, \code{\link{confIntProportion}},
+#' \code{\link[stats]{fisher.test}}, \code{\link[exact2x2]{exact2x2}}
 #' @references The Newcombe interval is introduced in
 #' 
 #' Newcombe, R.G. (1998). Interval estimation for the difference between
@@ -240,7 +240,16 @@ confIntIndependentProportion <- function(x, n, conf.level = 0.95)
 {
     ## Calculates confidence interval for risk difference of two independent
     ## samples based on individual Wilson intervals
-
+    stopifnot(is.numeric(x), length(x) == 2,
+              is.finite(x), is.wholenumber(x),
+              is.numeric(n), length(n) == 2,
+              is.finite(n), is.wholenumber(n),
+              x <= n, n >= 1,
+              is.numeric(conf.level),
+              length(conf.level) == 1,
+              is.finite(conf.level),
+              0 < conf.level, conf.level < 1)
+    
     alpha <- 1 - conf.level
 
     p1 <- x[1] / n[1]
@@ -265,3 +274,98 @@ confIntIndependentProportion <- function(x, n, conf.level = 0.95)
          "newcombeCI" = as.numeric(newcombe),
          "waldCI" = as.numeric(wald))
 }
+
+
+#' Confidence interval for the difference of paired binomial
+#' proportions using Newcombe's method
+#' 
+#' Compute confidence interval for the difference of paired binomial
+#' proportions using Newcombe's method.
+#' 
+#' 
+#' @aliases confIntPairedProportion
+#' @param x A two-dimensional contingency table in matrix form.
+#' @param conf.level Confidence level for confidence interval.
+#' @return A list with the entries:
+#' \item{p1}{Estimated proportion \eqn{p_{1+}}.}
+#' \item{p2}{Estimated proportion \eqn{p_{+1}}.}
+#' \item{newcombeCI}{Confidence interval for the difference of paired
+#' proportions, computed according to Newcombe (1998).}
+#' @author Kaspar Rufibach \cr \email{kaspar.rufibach@@gmail.com}
+#' @references The Newcombe interval is introduced in
+#' 
+#' Newcombe, R.G. (1998). Improved confidence intervals for the difference
+#' between binomial proportions based on paired data. \emph{Stat. Med.},
+#' \bold{17}, 2635--2650.
+#' 
+#' A worked out example can be found in
+#' 
+#' Altman, D.G., Machin, D., Bryant, T.N., Gardner, M.J. (2000). Statistics
+#' with confidence. University Press Belfast.
+#' @keywords univar htest
+#' @seealso \code{\link{confIntIndependentProportion}}, \code{\link{wilson}},
+#' \code{\link[exact2x2]{mcnemar.exact}}
+#' @examples
+#' 
+#' # Calculate confidence interval for the example in Altman et al (2000), Table 6.2
+#' altman62 <- rbind(c(14, 5), c(0, 22))
+#' confIntPairedProportion(x = altman62)
+#' 
+#' # exact McNemar test
+#' if(require("exact2x2")){
+#'     mcnemar.exact(altman62)
+#'     mcnemar.test(altman62)
+#' }
+#' 
+#' @export
+confIntPairedProportion <- function(x, conf.level = 0.95){
+   
+    ## confidence interval for a paired proportion, according to Newcombe (1998)
+    ## see Altman (2000), "Statistics with confidence", p. 52 for a worked out example
+
+    stopifnot(is.numeric(x), is.matrix(x), dim(x) == c(2, 2),
+              is.finite(x), is.wholenumber(x),
+              is.numeric(conf.level),
+              length(conf.level) == 1,
+              is.finite(conf.level),
+              0 < conf.level, conf.level < 1)
+    
+    r <- x[1, 1]
+    s <- x[1, 2]
+    t <- x[2, 1]
+    u <- x[2, 2]
+    n <- r + s + t + u
+    
+    p1 <- (r + s) / n
+    p2 <- (r + t) / n
+    D <- p1 - p2
+    
+    ## compute Wilson CIs for p1 and p2
+    ci1 <- wilson(x = r + s, n = n, conf.level = conf.level)
+    ci2 <- wilson(x = r + t, n = n, conf.level = conf.level)
+    l1 <- ci1[1]
+    u1 <- ci1[3]
+    l2 <- ci2[1]
+    u2 <- ci2[3]
+
+    phi <- 0
+    if (max(r + s, t + u, r + t, s + u) > 0){
+        A <- (r + s) * (t + u) * (r + t) * (s + u)
+        B <- r * u - s * t
+        C <- 0
+        if (B > n / 2){
+            C <- B - n / 2
+        }
+        if (B < 0){
+            C <- B
+        }
+        phi <- C / sqrt(A)    
+    }
+    
+    newcombe1 <- D - sqrt((p1 - l1) ^ 2 - 2 * phi * (p1 - l1) * (u2 - p2) + (u2 - p2) ^ 2)
+    newcombe2 <- D + sqrt((p2 - l2) ^ 2 - 2 * phi * (p2 - l2) * (u1 - p1) + (u1 - p1) ^ 2)
+    
+    newcombe <- c(newcombe1, newcombe2)
+    list("p1" = p1, "p2" = p2, "diff" = D, "newcombeCI" = as.numeric(newcombe))
+}
+
