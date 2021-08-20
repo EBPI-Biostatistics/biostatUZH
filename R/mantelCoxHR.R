7d8828adbe875be639636d1171ffe48961710171
Florian Gerber <flora.fauna.gerber@gmail.com>
2021-08-20 10:43:31 +0200

    mantel*

--- R/mantelCoxHR.R
+++ R/mantelCoxHR.R
@@ -11,84 +11,90 @@
 #' @param group Factor with two levels, e.g. treatment group.
 #' @param conf.level Significance level for confidence interval for hazard
 #' ratio.
-#' @return \item{mantelCox.hr}{Hazard ratio estimate.} \item{ci.hr}{Wald
-#' confidence interval at the level specified by \code{conf.level}.}
+#' @return \item{mantelCox.hr}{Hazard ratio estimate.}
+#' \item{ci.hr}{Wald confidence interval at the level specified by \code{conf.level}.}
 #' \item{p.val.logrank}{\eqn{p}-value of logrank test for a comparison of the
-#' survival curves between the two groups.} \item{coxph.hr}{Hazard ratio
-#' estimated via Cox-regression.}
-#' @note Note that in general the Mantel-Cox estimate and the hazard ratio
-#' estimate received via Cox-regression \emph{do not coincide}.
+#' survival curves between the two groups.}
+#' \item{coxph.hr}{Hazard ratio estimated via Cox-regression.}
+#' @note In general the Mantel-Cox estimate and the hazard ratio
+#' estimate received via Cox-regression do not coincide.
 #' @author Kaspar Rufibach \cr \email{kaspar.rufibach@@gmail.com}
 #' @references Kirkwood, B.R. and Sterne, J.A.C. (2003). \emph{Essential
 #' Medical Statistics}. Blackwell Science. See p. 283 ff.
 #' @keywords htest survival
 #' @examples
 #' 
-#' ## use Acute Myelogenous Leukemia survival data contained in package 'survival'
-#' time <- leukemia[, 1]; status <- leukemia[, 2]; x <- as.factor(leukemia[, 3])
-#' mantelCoxHR(time, status, x)
+#' ## load Acute Myelogenous Leukemia survival data
+#' data("leukemia", package = "survival")
+#' 
+#' mantelCoxHR(time = leukemia[, 1],
+#'             event = leukemia[, 2],
+#'             group = as.factor(leukemia[, 3]))
 #'
 #' @import survival
 #' @export
 mantelCoxHR <- function(time, event, group, conf.level = 0.95){
 
-## Compute HR according to Mantel-Cox formula
-## See Kirkwood & Sterne, p. 283.
-##
-## Kaspar Rufibach, November 2008
+    ## 'time', 'event', 'group' are checked in 'Surv()' and 'survfit()'
+    stopifnot(is.numeric(conf.level),
+              length(conf.level) == 1,
+              is.finite(conf.level),
+              0 < conf.level, conf.level < 1)
 
-alpha <- 1 - conf.level
-group <- as.factor(group)
-s.obj <- survfit(Surv(time, event) ~ group, conf.type = "none") 
-s <- summary(s.obj)
-dat <- data.frame(s$time, s$strata, s$n.risk, s$n.event) 
-dat <- dat[order(s$time), ] 
-lev <- levels(dat$s.strata)
-res <- matrix(NA, ncol = 4, nrow = length(dat[, 1]))
-
-for (i in 1:length(dat[, 1])){
-    tmp <- dat[i, ]
-    if (tmp[2] == lev[1]){res[i, 1:2] <- c(tmp[[3]], tmp[[4]])}
-    if (tmp[2] == lev[2]){res[i, 3:4] <- c(tmp[[3]], tmp[[4]])}
-}
-
-## res2 is the table on p. 285 in Kirkwood & Sterne
-res2 <- res 
-n2 <- length(res2[, 1])
-for (i in 1:n2){
-    g1 <- res2[i, 1:2]
-    if (is.na(g1[1]) == TRUE){res2[i, 1:2] <- c(sum(time[group == levels(group)[1]] >= dat$s.time[i], na.rm = TRUE), 0)}
-
-    g2 <- res2[i, 3:4]
-    if (is.na(g2[1]) == TRUE){res2[i, 3:4] <- c(sum(time[group == levels(group)[2]] >= dat$s.time[i], na.rm = TRUE), 0)}
-} 
-dimnames(res2)[[2]] <- c("n0i", "d0i", "n1i", "d1i")
-
-## compute Mantel-Cox HR
-h0i <- res2[, "n0i"] - res2[, "d0i"] 
-h1i <- res2[, "n1i"] - res2[, "d1i"]
-ni <- res2[, "n0i"] + res2[, "n1i"]
-di <- res2[, "d0i"] + res2[, "d1i"]
-hi <- h0i + h1i
-Q <- sum(res2[, "d1i"] * h0i / ni)
-R <- sum(res2[, "d0i"] * h1i / ni)
-U <- sum(res2[, "d1i"] - di * res2[, "n1i"] / ni)
-V <- sum(di * hi * res2[, "n1i"] * res2[, "n0i"] / (ni ^ 2 * (ni - 1)), na.rm = TRUE)
-hr <- Q / R
-
-## compute confidence interval
-se.log.hr <- sqrt(V / (Q * R))
-ci.hr <- exp(log(hr) + qnorm(c(alpha / 2, 1 - alpha / 2)) * se.log.hr)
-
-## p-value logrank test
-chi2 <- U ^ 2 / V
-p.val <- pchisq(chi2, df = 1, lower.tail = FALSE)
-
-## compare to HR computed from Cox-regression 
-    obj2 <- Surv(time, event) 
-    fit2 <- coxph(obj2 ~ group)
+    alpha <- 1 - conf.level
+    group <- as.factor(group)
+    s <- summary(survfit(Surv(time, event) ~ group, conf.type = "none") )
+    dat <- data.frame(s$time, s$strata, s$n.risk, s$n.event) 
+    dat <- dat[order(s$time), ] 
+    lev <- levels(dat$s.strata)
+    res <- matrix(NA, ncol = 4, nrow = nrow(dat))
+    
+    for (i in seq_len(nrow(dat))){
+        tmp <- dat[i, ]
+        if (tmp[2] == lev[1]){res[i, 1:2] <- c(tmp[[3]], tmp[[4]])}
+        if (tmp[2] == lev[2]){res[i, 3:4] <- c(tmp[[3]], tmp[[4]])}
+    }
+    
+    ## res2 is the table on p. 285 in Kirkwood & Sterne
+    res2 <- res 
+    n2 <- length(res2[, 1])
+    for (i in 1:n2){
+        g1 <- res2[i, 1:2]
+        if (is.na(g1[1]) == TRUE){
+            res2[i, 1:2] <- c(sum(time[group == levels(group)[1]] >= dat$s.time[i], na.rm = TRUE), 0)
+        }
+        
+        g2 <- res2[i, 3:4]
+        if (is.na(g2[1]) == TRUE){
+            res2[i, 3:4] <- c(sum(time[group == levels(group)[2]] >= dat$s.time[i], na.rm = TRUE), 0)
+        }
+    } 
+    dimnames(res2)[[2]] <- c("n0i", "d0i", "n1i", "d1i")
+    
+    ## compute Mantel-Cox HR
+    h0i <- res2[, "n0i"] - res2[, "d0i"] 
+    h1i <- res2[, "n1i"] - res2[, "d1i"]
+    ni <- res2[, "n0i"] + res2[, "n1i"]
+    di <- res2[, "d0i"] + res2[, "d1i"]
+    hi <- h0i + h1i
+    Q <- sum(res2[, "d1i"] * h0i / ni)
+    R <- sum(res2[, "d0i"] * h1i / ni)
+    U <- sum(res2[, "d1i"] - di * res2[, "n1i"] / ni)
+    V <- sum(di * hi * res2[, "n1i"] * res2[, "n0i"] / (ni ^ 2 * (ni - 1)), na.rm = TRUE)
+    hr <- Q / R
+    
+    ## compute confidence interval
+    se.log.hr <- sqrt(V / (Q * R))
+    ci.hr <- exp(log(hr) + qnorm(c(alpha / 2, 1 - alpha / 2)) * se.log.hr)
+    
+    ## p-value logrank test
+    chi2 <- U ^ 2 / V
+    p.val <- pchisq(chi2, df = 1, lower.tail = FALSE)
+    
+    ## compare to HR computed from Cox-regression 
+    fit2 <- coxph(Surv(time, event) ~ group)
     cfit2 <- as.numeric(coef(fit2))
     hr.cox <- exp(cfit2)
-
-return(list("mantelCox.hr" = hr, "ci.hr" = ci.hr, "p.val.logrank" = p.val, "coxph.hr" = hr.cox))
+    
+    list("mantelCox.hr" = hr, "ci.hr" = ci.hr, "p.val.logrank" = p.val, "coxph.hr" = hr.cox)
 }
--- R/mantelHaenszel.R
+++ R/mantelHaenszel.R
@@ -8,36 +8,38 @@
 #' @param exposure Binary variable coding whether patient was exposed (1) or
 #' not (0).
 #' @param outcome Binary variable coding outcome of patient.
-#' @param stratum Nominal variable containing information about matching, i.e.
-#' strata.
+#' @param stratum Factor object with at least 2 levels identifying to which
+#' stratum the corresponding elements in \code{exposure} and \code{outcome} belong.
 #' @return \item{tab}{Table that counts numbers of strata for each case-control
-#' combination.} \item{test.stat}{Test statistic for \eqn{chi^2} test.}
+#' combination.}
+#' \item{test.stat}{Test statistic for \eqn{chi^2} test.}
 #' \item{p.val}{\eqn{p}-value of \eqn{chi^2} test.}
 #' @author Kaspar Rufibach \cr \email{kaspar.rufibach@@gmail.com}
-#' @seealso Similar functionality is provided in \code{\link{mantelhaen.test}}
-#' and \code{clogit} in \pkg{survival}. See the examples below for a
+#' @seealso Similar functionality is provided in \code{\link[stats]{mantelhaen.test}}
+#' and \code{\link[survival]{clogit}}. See the examples below for a
 #' comparison.
-#' @references Agresti, A. (2002). \emph{Categorical data analysis}. New York:
-#' Wiley.
+#' @references Agresti, A. (2002). \emph{Categorical data analysis}. Wiley, New York,
 #' @keywords htest survival
 #' @examples
 #' 
-#' 
 #' # generate data
 #' set.seed(1977)
-#' exposure <- rep(c(1, 0, 0, 0, 0), 41)
-#' outcome <- sample(c(rep(1, 62), rep(0, 5 * 41 - 62)))
-#' strata <- rep(1:41, each = 5)
+#' data <- data.frame(exposure = rep(c(1, 0, 0, 0, 0), 41),
+#'                    outcome = sample(x = c(rep(1, 62), rep(0, 5 * 41 - 62))),
+#'                    stratum = rep(1:41, each = 5))
 #' 
 #' # via conditional logistic regression
-#' logreg <- clogit(outcome ~ exposure + strata(strata), method = "approximate")
+#' logreg <- clogit(outcome ~ exposure + strata(stratum), method = "approximate",
+#'                  data = data)
 #' summary(logreg)
 #' 
 #' # R function in library 'stats'
-#' mh <- mantelhaen.test(x = outcome, y = exposure, z = strata)
+#' mh <- with(data, mantelhaen.test(x = outcome, y = exposure, z = stratum))
 #' 
 #' # this function
-#' mH <- mantelHaenszel(exposure, outcome, strata)
+#' mH <- with(data, mantelHaenszel(exposure = exposure,
+#'                                 outcome = outcome,
+#'                                 stratum = stratum))
 #' 
 #' # compare p-values
 #' summary(logreg)$coef[5]
@@ -46,40 +48,50 @@
 #' 
 #' @export
 mantelHaenszel <- function(exposure, outcome, stratum){
-
-ss <- unique(stratum)
-tab <- matrix(0, ncol = table(stratum)[1], nrow = 2)
-M <- matrix(ncol = 3, nrow = length(ss))
+    stopifnot(is.numeric(exposure),
+              length(exposure) > 0,
+              is.finite(exposure),
+              exposure %in% c(0, 1),
+              is.numeric(outcome),
+              length(outcome) == length(exposure),
+              is.finite(outcome),
+              outcome %in% c(0, 1))
+    stratum <- as.numeric(stratum)
+    stopifnot(length(stratum) == length(exposure),
+              is.finite(stratum),
+              length(unique(stratum)) >= 2)
     
-for (s in ss){
+    ss <- unique(stratum)
+    tab <- matrix(0, ncol = table(stratum)[1], nrow = 2)
+    M <- matrix(ncol = 3, nrow = length(ss))
     
-    resp.strat <- outcome[stratum == s]
-    expo.strat <- exposure[stratum == s]
-
-    # generate Mantel-Haenszel table
-    resp.case <- resp.strat[expo.strat == 1]
-    resp.control <- sum(resp.strat[expo.strat == 0])
-    tab[2 - resp.case, resp.control + 1] <- 1 + tab[2 - resp.case, resp.control + 1]
-
-    # manual computation of MH test statistic
-    N <- matrix(NA, 2, 2)
-    N[1, 1] <- sum((resp.strat == 1) * (expo.strat == 1))
-    N[1, 2] <- sum((resp.strat == 1) * (expo.strat == 0))
-    N[2, 1] <- sum((resp.strat == 0) * (expo.strat == 1))    
-    N[2, 2] <- sum((resp.strat == 0) * (expo.strat == 0))
+    for (s in ss){
+        resp.strat <- outcome[stratum == s]
+        expo.strat <- exposure[stratum == s]
+        
+                                        # generate Mantel-Haenszel table
+        resp.case <- resp.strat[expo.strat == 1]
+        resp.control <- sum(resp.strat[expo.strat == 0])
+        tab[2 - resp.case, resp.control + 1] <- 1 + tab[2 - resp.case, resp.control + 1]
+        
+                                        # manual computation of MH test statistic
+        N <- matrix(NA, 2, 2)
+        N[1, 1] <- sum((resp.strat == 1) * (expo.strat == 1))
+        N[1, 2] <- sum((resp.strat == 1) * (expo.strat == 0))
+        N[2, 1] <- sum((resp.strat == 0) * (expo.strat == 1))    
+        N[2, 2] <- sum((resp.strat == 0) * (expo.strat == 0))
+        
+        m11 <- sum(N[1, ]) * sum(N[, 1]) / sum(N)
+        Vn11 <- sum(N[1, ]) * sum(N[2, ]) * sum(N[, 1]) * sum(N[, 2]) / (sum(N) ^ 2 * (sum(N) - 1))           
+        
+        M[s, ] <- c(N[1, 1], m11, Vn11)
+    }
     
-    m11 <- sum(N[1, ]) * sum(N[, 1]) / sum(N)
-    Vn11 <- sum(N[1, ]) * sum(N[2, ]) * sum(N[, 1]) * sum(N[, 2]) / (sum(N) ^ 2 * (sum(N) - 1))           
-
-    M[s, ] <- c(N[1, 1], m11, Vn11)
-}
-
-test.stat <- (abs(sum(M[, 1] - M[, 2])) - 0.5) ^ 2 / sum(M[, 3])
-p.val <- 1 - pchisq(test.stat, df = 1)
-
-dimnames(tab) <- list(c("Case exposure yes", "Case exposure no"), rep(0:4))
-res <- list("tab" = tab, "test.stat" = test.stat, "p.val" = p.val)
-return(res)
+    test.stat <- (abs(sum(M[, 1] - M[, 2])) - 0.5) ^ 2 / sum(M[, 3])
+    p.val <- 1 - pchisq(test.stat, df = 1)
+    
+    dimnames(tab) <- list(c("Case exposure yes", "Case exposure no"), rep(0:4))
+    list("tab" = tab, "test.stat" = test.stat, "p.val" = p.val)
 }
 
 
