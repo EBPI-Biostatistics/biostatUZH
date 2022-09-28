78fd736b604f25b5b691c60b10827fc8f7cde1f0
Felix Hofmann <felix_hofmann_13@hotmail.com>
2022-09-28 17:22:40 +0200

    Reexport FUNS

---
+++ R/reexports.R
@@ -0,0 +1,23 @@
+#' @importFrom ReplicationSuccess ci2p
+#' @export
+ReplicationSuccess::ci2p
+
+#' @importFrom ReplicationSuccess ci2estimate
+#' @export
+ReplicationSuccess::ci2estimate
+
+#' @importFrom ReplicationSuccess ci2se
+#' @export
+ReplicationSuccess::ci2se
+
+#' @importFrom ReplicationSuccess ci2z
+#' @export
+ReplicationSuccess::ci2z
+
+#' @importFrom ReplicationSuccess z2p
+#' @export
+ReplicationSuccess::z2p
+
+#' @importFrom ReplicationSuccess p2z
+#' @export
+ReplicationSuccess::p2z
